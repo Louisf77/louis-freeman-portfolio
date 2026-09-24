356bc2fe05d7c9934669f263fbf33ce917fb2db1
Louis Freeman <louisfreeman@Louiss-Mac-mini.communityfibre.co.uk>
2026-09-24 11:04:18 +0100

    Log malformed consent cookies with the error class and message

--- app/models/cookie_consent.rb
+++ app/models/cookie_consent.rb
@@ -2,11 +2,12 @@ class CookieConsent
   COOKIE_NAME = "cookie_consent".freeze
   VERSION = 1
 
-  def self.from_cookie(value:)
+  def self.from_cookie(value:, logger: Rails.logger)
     stored_choice = value.present? ? JSON.parse(value) : {}
     stored_choice = {} unless stored_choice.is_a?(Hash)
     new(analytics: stored_choice["analytics"] == true, version: stored_choice["version"])
-  rescue JSON::ParserError
+  rescue JSON::ParserError => e
+    logger.warn("CookieConsent: ignoring malformed cookie (#{e.class}: #{e.message})")
     new(analytics: false, version: nil)
   end
 
