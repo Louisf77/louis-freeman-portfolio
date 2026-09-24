6f931f4f5bbf1bc506f9210423bd99fa63d82ed7
Louis Freeman <84720791+Louisf77@users.noreply.github.com>
2026-09-24 08:40:06 +0100

    Add lint tooling configured to the coding preferences

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
-  def page_bootstrap_json
-    json_escape({ queries: {}, ui: t("ui") }.to_json).html_safe
+  def page_bootstrap_script_tag
+    tag.script(json_escape({ queries: {}, ui: t("ui") }.to_json).html_safe, id: "bootstrap", type: "application/json")
   end
 end
